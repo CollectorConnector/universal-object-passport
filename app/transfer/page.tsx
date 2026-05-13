b72ebed8c2d6557cd5983b927efeffd723c11b3d
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-05-13 22:22:50 +0100

    Create TransferPage component for ownership transfer

---
+++ app/transfer/page.tsx
@@ -0,0 +1,11 @@
+export default function TransferPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold">Transfer Ownership</h1>
+      <p className="mt-2 text-gray-600">
+        Generate a secure transfer link for the new owner.
+      </p>
+      {/* Ownership transfer form */}
+    </main>
+  );
+}
